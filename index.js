409da843999e4fa5011b73f5ef5c340c08fd5d99
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-04-09 11:24:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ let gifhyAPI =
 async function getData() {
   let res = await fetch(randomWordUrl);
   let data = await res.json();
-  console.log(data[0]);
   let resGifhy = await fetch(gifhyAPI + `&q=${data[0]}&limit=5`);
   let dataGifhy = await resGifhy.json();
-  console.log(dataGifhy.data[0]);
   let object = document.createElement("object");
-  object.data = dataGifhy.data ? dataGifhy.data[0].images.original.url : null;
-  if (object.data === null) {
-    // object.innerHTML = `<p>No data Found for the search word ${data[0]}<p>`;
-    let h2 = document.createElement("h2");
-    h2.innerText = `No data Found for the search word ${data[0]}`;
-    document.body.append(h2);
+  if (dataGifhy.data.length === 0) {
+    let h1 = document.createElement("h1");
+    h1.setAttribute("style", "text-align:center;margin-top:100px");
+    h1.innerHTML = `No data Found for the search word <i>${data[0]}</i>`;
+    document.body.append(h1);
   } else {
+    object.data = dataGifhy.data[0].images.original.url;
     object.height = "700px";
     object.width = "100%";
     document.body.append(object);
